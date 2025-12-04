6057fe0ffc25bf9b98335f03315f4bf1f07dbba1
sheraz61 <hsheraz271@gmail.com>
2025-12-04 21:34:54 +0500

    creating proxy to request backend also complete signup UI and connect UI with backend controller

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  server:{
+    proxy:{
+      '/api':{
+        target:'http://localhost:3000',
+        secure:false,
+        
+      }
+    }
+  },
   plugins: [react()
     , tailwindcss(),
   ],
