c12eb96bd62ea40a7e1d75581ef397b10a6859af
sheraz61 <hsheraz271@gmail.com>
2025-12-13 15:10:59 +0500

    add update listing functionality controller

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -45,3 +45,44 @@ export const deleteListing = async (req, res, next) => {
     next(error)
   }
 }
+
+export const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, 'Listing not found'))
+    }
+    if (req.user.id !== listing.userRef) {
+      return next(errorHandler(401, 'You only edit if u are autherized'))
+    }
+    if (req.files && req.files.length > 0) {
+      if (listing.imagePublicIds && listing.imagePublicIds.length > 0) {
+        const deletePromises = listing.imagePublicIds.map(async (publicId) => {
+          return await cloudinary.uploader.destroy(publicId)
+        })
+        await Promise.all(deletePromises)
+      }
+      const newImageUrls = req.files.map(file => file.path)
+      const newImagePublicIds = req.files.map(file => file.filename);
+      const updateListing = await Listing.findByIdAndUpdate(
+        req.params.id,
+        {
+          ...req.body,
+          imageUrls: newImageUrls,
+          imagePublicIds: newImagePublicIds
+        },
+        { new: true }
+      )
+      return res.status(200).json(updateListing)
+    }
+    const updatedListing = await Listing.findByIdAndUpdate(
+      id.params.id,
+      req.body,
+      { new: true }
+    )
+    res.status(200).json(updatedListing)
+
+  } catch (error) {
+    next(error)
+  }
+}
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { createListing,deleteListing } from '../controllers/listing.controller.js'
+import { createListing,deleteListing ,updateListing} from '../controllers/listing.controller.js'
 import { verifyToken } from '../middlewares/auth.js'
 import upload from '../middlewares/multer.js';
 import { setUploadTarget } from '../middlewares/setUploadTarget.js'
 const router = express.Router()
 
+router.post('/update/:id',verifyToken,updateListing)
 router.post('/create',
      verifyToken, 
      setUploadTarget('listing'),
