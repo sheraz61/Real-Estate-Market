be3dc5d13c4b91bbcfd58465cb433afacbdaf851
sheraz61 <hsheraz271@gmail.com>
2025-12-13 22:50:59 +0500

    complete home page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,8 +28,8 @@ function Header() {
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>sheraz</span>
-            <span className='text-slate-700'>estate</span>
+            <span className='text-slate-500'>Sheraz</span> 
+            <span className='text-slate-700'>-estate</span>
           </h1>
         </Link>
         <form
